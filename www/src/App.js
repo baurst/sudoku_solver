c72ea88e1dd2ac24c83d2f080f1901aa379f61c8
baurst <stefan.a.baur@gmail.com>
2020-04-07 22:27:37 +0200

    Test.tsx works
    Branch: integrate_react
    
    Branch: (no branch, rebasing integrate_react)

--- www/src/App.js
+++ www/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Test from './Test'
 
 class App extends React.PureComponent {
 	render() {
 		return (
 			<div>
 				<h1>Hello World!</h1>
+				<Test />
 			</div>
 		);
 	}
