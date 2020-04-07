664096941a18023c6f978ca7618e8af7b0b5148d
baurst <stefan.a.baur@gmail.com>
2020-04-07 22:18:26 +0200

    it werks
    Branch: integrate_react

--- www/src/index.js
+++ www/src/index.js
@@ -1,8 +1,8 @@
-// import * as wasm from "sudoku_solver";
+import * as wasm from "sudoku_solver";
 
-// var a = wasm.wasm_solve_sudoku("006037508700010900130050020002908000050020430600000090200005704003100060498600000");
+var a = wasm.wasm_solve_sudoku("006037508700010900130050020002908000050020430600000090200005704003100060498600000");
 
-// console.log(a);
+console.log(a);
 
 import React from 'react';
 import ReactDOM from 'react-dom';
