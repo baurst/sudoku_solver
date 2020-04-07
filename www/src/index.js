7eccddf127a6b6d49a1750e811b89cde136acc44
baurst <stefan.a.baur@gmail.com>
2020-04-07 22:08:03 +0200

    hello world works
    Branch: integrate_react

---
+++ www/src/index.js
@@ -0,0 +1,10 @@
+// import * as wasm from "sudoku_solver";
+
+// var a = wasm.wasm_solve_sudoku("006037508700010900130050020002908000050020430600000090200005704003100060498600000");
+
+// console.log(a);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(<><App /></>, document.getElementById('root'));
