cfc990b313dfb2b261036dbd3003f5fe59ff801a
baurst <stefan.a.baur@gmail.com>
2020-04-14 16:04:59 +0200

    Updates
    Branch: gh-pages

--- bootstrap.js
+++ bootstrap.js
@@ -155,7 +155,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/sudoku_solver_bg.wasm":"32c9fd4002b557dc30ac"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/sudoku_solver_bg.wasm":"60ce44dfaf9689e019a8"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
